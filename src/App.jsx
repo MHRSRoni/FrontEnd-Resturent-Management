3343fc74d996f821f3d8212761504d2461fe2d71
sumonbarai <sumonbarai78@gmail.com>
2023-09-19 00:49:52 +0600

    daisy ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-500">Hello world!</h1>
+    <div>
+      <Home />
+    </div>
   );
 }
 
