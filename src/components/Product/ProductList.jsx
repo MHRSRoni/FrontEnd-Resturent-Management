eae8fa4f850cf69218c87299a1d2ca4b52246385
sumonbarai <sumonbarai78@gmail.com>
2023-09-27 18:50:32 +0600

    pagination added in menupage

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -7,9 +7,11 @@ import { BASE_URL } from "../../App";
 import ProductCard from "./ProductCard";
 import CardSalton from "../Line-Loader/CardSalton";
 import Category from "../Category/Category";
+import ReactPaginate from "react-paginate";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
+  const [totalPage, setTotalPage] = useState(1);
   const [loading, setLoading] = useState(true);
   const [category, setCategory] = useState("");
   const [input, setInput] = useState("");
@@ -20,6 +22,7 @@ function ProductList() {
     setSearchValue("");
     setInput("");
     setCategory(value);
+    setPageNumber(1);
   };
 
   const handleSearch = (e) => {
@@ -27,14 +30,15 @@ function ProductList() {
     const val = input;
     setSearchValue(val.toLowerCase());
     setCategory("");
+    setPageNumber(1);
   };
 
   // Url define
   let URL;
   if (searchValue) {
-    URL = `${BASE_URL}/food?search=${searchValue}&pageNumber=1`;
+    URL = `${BASE_URL}/food?search=${searchValue}&pageNumber=${pageNumber}`;
   } else if (category) {
-    URL = `${BASE_URL}/food?category=${category}&pageNumber=1`;
+    URL = `${BASE_URL}/food?category=${category}&pageNumber=${pageNumber}`;
   } else {
     URL = `${BASE_URL}/food?pageNumber=${pageNumber}`;
   }
@@ -44,8 +48,10 @@ function ProductList() {
     axios
       .get(URL)
       .then((response) => {
+        console.log(response);
         if (response.status === 200) {
           setProducts(response?.data?.data);
+          setTotalPage(response?.data?.totalPage);
         } else {
           searchValue([]);
         }
@@ -56,34 +62,61 @@ function ProductList() {
       .finally(() => {
         setLoading(false);
       });
-  }, [searchValue, category]);
+  }, [searchValue, category, pageNumber, URL]);
+
+  const handlePageClick = (data) => {
+    setPageNumber(data.selected + 1);
+  };
 
   return (
-    <div className="py-2 px-4">
-      <SearchBar
-        onChangeHandler={handleSearch}
-        input={input}
-        setInput={setInput}
-      />
-      <Category onChangeHandler={handleChangeCategory} />
-      <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-8">
-        {products.length !== 0 ? (
-          products.map((item) => <ProductCard key={item._id} item={item} />)
-        ) : (
-          <>
-            <CardSalton />
-            <CardSalton />
-            <CardSalton />
-            <CardSalton />
-            <CardSalton />
-            <CardSalton />
-            <CardSalton />
-            <CardSalton />
-          </>
-        )}
+    <>
+      <div className="py-2 px-4">
+        <SearchBar
+          onChangeHandler={handleSearch}
+          input={input}
+          setInput={setInput}
+        />
+        <Category onChangeHandler={handleChangeCategory} />
+        <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-8">
+          {products.length !== 0 ? (
+            products.map((item) => <ProductCard key={item._id} item={item} />)
+          ) : (
+            <>
+              <CardSalton />
+              <CardSalton />
+              <CardSalton />
+              <CardSalton />
+              <CardSalton />
+              <CardSalton />
+              <CardSalton />
+              <CardSalton />
+            </>
+          )}
+        </div>
+        {loading && <Loader />}
       </div>
-      {loading && <Loader />}
-    </div>
+      <ReactPaginate
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        pageCount={totalPage}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={handlePageClick}
+        containerClassName={"list-none flex justify-center items-center mt-10"}
+        pageClassName={"bg-neutral cursor-pointer"}
+        pageLinkClassName={"p-3 inline-block"}
+        previousClassName={
+          "bg-neutral cursor-pointer rounded-tl-lg rounded-bl-lg"
+        }
+        previousLinkClassName={"p-3 inline-block"}
+        nextClassName={"bg-neutral cursor-pointer rounded-tr-lg rounded-br-lg"}
+        nextLinkClassName={"p-3 inline-block"}
+        breakClassName={"bg-neutral cursor-pointer"}
+        breakLinkClassName={"p-3 inline-block"}
+        activeClassName={"active"}
+      />
+    </>
   );
 }
 
