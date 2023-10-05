8e258f3c0c4ffc2503065e21c713c142141fd4b7
sumonbarai <sumonbarai78@gmail.com>
2023-10-05 14:01:08 +0600

    [proccesing] some modify login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,80 +1,87 @@
-
-
 import { useState } from "react";
 import Button from "../components/ui/Button";
 import { useAuth } from "../contexts/AuthProvider";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [auth, setAuth] = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [auth, setAuth] = useAuth();
 
-    const navigate = useNavigate();
-    const location = useLocation();
-    console.log(location)
+  const navigate = useNavigate();
+  const location = useLocation();
+  console.log(location);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const { data } = await axios.post('/login', { email, password });
-            if (data?.error) {
-                alert(data.error);
-            } else {
-                localStorage.setItem("auth", JSON.stringify(data));
-                setAuth({ ...auth, token: data.token, user: data.user });
-                alert("Login Successful");
-                navigate('/');
-                // navigate(location.state ||
-                //     `/dashboard/${data?.user?.role === 1 ? "admin" : "user"}`
-                // );
-
-            }
-        } catch (error) {
-            console.log(error);
-            alert("Login Failed Try Again")
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/login", { email, password });
+      if (data?.error) {
+        alert(data.error);
+      } else {
+        localStorage.setItem("auth", JSON.stringify(data));
+        setAuth({ ...auth, token: data.token, user: data.user });
+        alert("Login Successful");
+        navigate("/");
+        // navigate(location.state ||
+        //     `/dashboard/${data?.user?.role === 1 ? "admin" : "user"}`
+        // );
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Login Failed Try Again");
     }
+  };
 
+  return (
+    <section
+      style={{ height: "calc(100vh - 80px)" }}
+      className="text-center py-10 flex justify-center items-center"
+    >
+      <div className="w-3/12 shadow-2xl mx-auto py-10">
+        <h2 className="font-bold text-2xl text-slate-950 mb-3">Log In</h2>
+        <div className="w-32 mx-auto h-0.5 mb-5 mt-0 bg-orange-300"></div>
+        <form onSubmit={handleSubmit} className="mx-auto">
+          <div className="form-control w-full max-w-xs mx-auto mb-3">
+            <h2 className="text-left text-slate-950 mb-4">Email</h2>
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="input input-bordered w-full max-w-xs mx-auto"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs mx-auto mb-3">
+            <h2 className="text-left text-slate-950 mb-4">Password </h2>
 
-    return (
-        <section style={{ height: "calc(100vh - 80px)" }} className="text-center py-10 flex justify-center items-center" >
-            <div
-
-                className='w-3/12 shadow-2xl mx-auto py-10'>
-                <h2 className='font-bold text-2xl text-slate-950 mb-3'>Log In</h2>
-                <div className='w-32 mx-auto h-0.5 mb-5 mt-0 bg-orange-300'></div>
-                <form onSubmit={handleSubmit} className='mx-auto'>
-                    <div className="form-control w-full max-w-xs mx-auto mb-3">
-                        <h2 className='text-left text-slate-950 mb-4'>Email</h2>
-                        <input
-                            type="email"
-                            placeholder="Enter Your Email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="input input-bordered w-full max-w-xs mx-auto"
-                        />
-
-                    </div>
-                    <div className="form-control w-full max-w-xs mx-auto mb-3">
-                        <h2 className='text-left text-slate-950 mb-4'>Password </h2>
-
-                        <input
-                            type="password"
-                            placeholder="Enter Your Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="input input-bordered w-full max-w-xs mx-auto"
-                        />
-                    </div>
-                    <div className="form-control w-24 mx-auto mt-7">
-                        <Button variant="basic" size="normal" type="button" text=" Log In" />
-                    </div>
-                </form>
-            </div>
-        </section>
-    );
+            <input
+              type="password"
+              placeholder="Enter Your Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input input-bordered w-full max-w-xs mx-auto"
+            />
+          </div>
+          <div className="form-control w-24 mx-auto mt-7">
+            <Button
+              variant="basic"
+              size="normal"
+              type="button"
+              text=" Log In"
+            />
+          </div>
+          <h3 className="mt-3">
+            Create new account?
+            <span className="text-orange-500">
+              <Link to="/register"> Register</Link>
+            </span>{" "}
+          </h3>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,102 +1,103 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import { useAuth } from '../contexts/AuthProvider';
-import axios from 'axios';
+import { useAuth } from "../contexts/AuthProvider";
+import axios from "axios";
 
 import Button from "../components/ui/Button";
 
 const Register = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [auth, setAuth] = useAuth();
-    const navigate = useNavigate()
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [auth, setAuth] = useAuth();
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const { data } = await axios.post("/register", { name, email, password });
-            console.log(data);
-            if (data?.error) {
-                alert(data.error);
-            } else {
-                localStorage.setItem("auth", JSON.stringify(data));
-                setAuth({ ...auth, token: data.token, user: data.user });
-                alert("Registration Successful");
-                navigate('/')
-            }
-
-        } catch (error) {
-            console.log(error);
-            alert("Registration Failed. Try Again")
-        }
-
+    try {
+      const { data } = await axios.post("/register", { name, email, password });
+      console.log(data);
+      if (data?.error) {
+        alert(data.error);
+      } else {
+        localStorage.setItem("auth", JSON.stringify(data));
+        setAuth({ ...auth, token: data.token, user: data.user });
+        alert("Registration Successful");
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Registration Failed. Try Again");
     }
-    console.log(name, email, password)
-    return (
-        <section style={{ height: "calc(100vh - 80px)" }} className="text-center py-10 flex justify-center items-center">
-            <div
+  };
+  console.log(name, email, password);
+  return (
+    <section
+      style={{ height: "calc(100vh - 80px)" }}
+      className="text-center py-10 flex justify-center items-center"
+    >
+      <div className="w-3/12 mx-auto py-10 shadow-2xl ">
+        <h2 className="font-bold text-2xl text-black mb-6">Register</h2>
+        <div className="w-32 mx-auto h-0.5 mb-5 mt-0 bg-orange-500"></div>
+        <form onSubmit={handleSubmit} className="mx-auto">
+          <div className="form-control w-full max-w-xs mx-auto mb-3">
+            <h2 className="text-left text-gray-500 mb-4">User Name</h2>
 
-                className='w-3/12 mx-auto py-10 shadow-2xl '>
-                <h2 className='font-bold text-2xl text-black mb-6'>Register</h2>
-                <div className='w-32 mx-auto h-0.5 mb-5 mt-0 bg-orange-500'></div>
-                <form onSubmit={handleSubmit} className='mx-auto'>
-                    <div className="form-control w-full max-w-xs mx-auto mb-3">
-                        <h2 className='text-left text-gray-500 mb-4'>User Name</h2>
+            <input
+              type="text"
+              placeholder="Your Name"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="input input-bordered w-full max-w-xs mx-auto"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs mx-auto mb-3">
+            <h2 className="text-left text-gray-500 mb-4">Email </h2>
 
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="input input-bordered w-full max-w-xs mx-auto"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs mx-auto mb-3">
+            <h2 className="text-left text-gray-500 mb-4">Password </h2>
 
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input input-bordered w-full max-w-xs mx-auto"
+            />
+          </div>
+          <div className="form-control w-24 mx-auto mt-7">
+            <Button
+              variant="basic"
+              size="normal"
+              type="button"
+              text=" Register"
+            />
+          </div>
 
-
-
-                        <input
-                            type="text"
-                            placeholder="Your Name"
-                            name="name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            className="input input-bordered w-full max-w-xs mx-auto" />
-
-                    </div>
-                    <div className="form-control w-full max-w-xs mx-auto mb-3">
-                        <h2 className='text-left text-gray-500 mb-4'>Email </h2>
-
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            name="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="input input-bordered w-full max-w-xs mx-auto" />
-
-                    </div>
-                    <div className="form-control w-full max-w-xs mx-auto mb-3">
-                        <h2 className='text-left text-gray-500 mb-4'>Password </h2>
-
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            name="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="input input-bordered w-full max-w-xs mx-auto" />
-
-                    </div>
-                    <div className="form-control w-24 mx-auto mt-7">
-                        <Button variant="basic" size="normal" type="button" text=" Register" />
-                    </div>
-
-                    <h3 className='mt-3'> Already have an account? <span className="text-orange-500"> <Link to='/sign-up'> Login</Link></span> </h3>
-
-                </form>
-            </div>
-        </section>
-    );
-
-
-
-
+          <h3 className="mt-3">
+            {" "}
+            Already have an account?{" "}
+            <span className="text-orange-500">
+              <Link to="/login"> Login</Link>
+            </span>{" "}
+          </h3>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Register;
