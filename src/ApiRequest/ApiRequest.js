18eb2bb7aee5dc033d79f903fa4f50ff64df614a
sumonbarai <sumonbarai78@gmail.com>
2023-10-13 18:48:15 +0600

    register and otp modification

---
+++ src/ApiRequest/ApiRequest.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+export const registerRequest = async (formData) => {
+  try {
+    const response = await axios.post("/v2/customer/register", formData);
+    return {
+      data: response.data,
+      status: response.status,
+    };
+  } catch (error) {
+    return {
+      data: error.response.data,
+      status: error.response.status,
+    };
+  }
+};
+
+export const emailVerificationRequest = async (email) => {
+  try {
+    const response = await axios.post("/v2/customer/send-verification", {
+      emailSubject: "OTP Verification",
+      email: email,
+    });
+
+    return {
+      data: response.data,
+      status: response.status,
+    };
+  } catch (error) {
+    return {
+      data: error.response.data,
+      status: error.response.status,
+    };
+  }
+};
+
+export const otpVerifyRequest = async (RegEmail, OTP_code) => {
+  try {
+    const response = await axios.get(
+      `/v2/customer/auth/verify?email=${RegEmail}&otp=${OTP_code}`
+    );
+    return {
+      data: response.data,
+      status: response.status,
+    };
+  } catch (error) {
+    return {
+      data: error.response.data,
+      status: error.response.status,
+    };
+  }
+};
